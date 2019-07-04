30b2d981df4191345fb558b53efd81d90ed16e3c
SoheiWatanabe <s0he1watanabe@gmail.com>
2019-07-04 16:58:42 +0900

    データの保存形式を変更

--- SD.ino
+++ SD.ino
@@ -8,21 +8,15 @@
  ------------------------------------------------------------------*/
  
 void SaveToSDcard(){
-  datafile.println();
-  datafile.print(gps.satellites.value()); datafile.print(",");
-  datafile.print(gps.hdop.hdop()); datafile.print(",");
-  datafile.print(gps.location.lat(), 6); datafile.print(",");
-  datafile.print(gps.location.lng(), 6); datafile.print(",");
-  datafile.print(gps.location.age()); datafile.print(",");
+  datafile.println():
+  datafile.print(gps.date.year()); datafile.print(",");
   datafile.print(gps.date.month()); datafile.print(",");
   datafile.print(gps.date.day()); datafile.print(",");
-  datafile.print(gps.date.year()); datafile.print(",");
   datafile.print(gps.time.hour() + 9); datafile.print(",");//日本時間に変換
   datafile.print(gps.time.minute()); datafile.print(",");
   datafile.print(gps.time.second()); datafile.print(",");
-  datafile.print(gps.altitude.meters()); datafile.print(",");
-  datafile.print(gps.course.deg()); datafile.print(",");
-  datafile.print(gps.speed.kmph()); datafile.print(",");
+  datafile.print(gps.location.lat(), 6); datafile.print(",");
+  datafile.print(gps.location.lng(), 6); datafile.print(",");
   datafile.print(TinyGPSPlus::cardinal(gps.course.deg())); datafile.print(",");
   datafile.print(distanceKmToGoal); datafile.print(",");
   datafile.print(courseToGoal); datafile.print(",");
